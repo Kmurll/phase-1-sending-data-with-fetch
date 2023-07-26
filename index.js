d0db2c39b20e1417b56e886497ce88af28f57c10
Kelvin Muli Mue <kelvinmue@gmail.com>
2023-07-26 17:16:24 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("error")
+      }
+      return response.json()
+    })
+    .then(data => {
+      // Access the new id from the response and append it to the DOM
+      const id = data.id
+      const idElement = document.createElement("p")
+      idElement.textContent = `New user ID: ${id}`
+      document.getElementById("result").appendChild(idElement)
+    })
+    .catch(error => {
+      // Handle error and append the error message to the DOM
+      const errorElement = document.createElement("p")
+      errorElement.textContent = `Error: ${error.message}`
+      document.getElementById("result").appendChild(errorElement)
+    })
+  }
+  
+  document.getElementById("dataForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+  
+    submitData(name, email)
+      .then(() => {
+        // If successful, remove the form and display the success message
+        const form = document.getElementById("dataForm")
+        form.remove()
+  
+        const successMessage = document.createElement("p")
+        successMessage.textContent = "Form submitted successfully!"
+        document.getElementById("result").appendChild(successMessage)
+      })
+      .catch(error => {
+        // Display the error message on the page
+        const errorMessage = document.createElement("p")
+        errorMessage.textContent = "Error: " + error.message
+        document.getElementById("result").appendChild(errorMessage)
+      })
+  })
+  
